f07296434a83012b3c45bcaa85e8613a795f65d6
masete <nicholasmasete72@gmail.com>
2022-06-27 17:29:56 +0300

    why we need hooks

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = 'random title';
+  const handleOnClick = () => {
+    return(
+      console.log(title)
+    );
+  }
+  return (
+    <React.Fragment>
+      <h1>{title}</h1>
+      <button type='button' className='btn' onClick={handleOnClick}>Change Title</button>
+    </React.Fragment>
+  )
 };
 
 export default ErrorExample;
