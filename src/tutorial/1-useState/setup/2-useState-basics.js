226d5521a44b68ab04e312d355173b710e9e06f0
masete <nicholasmasete72@gmail.com>
2022-06-27 18:38:58 +0300

    hooks works at first exaple

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  console.log(useState('hello world'));
-  const value = useState(1)[0];
-  const handler = useState(1)[1];
-  console.log(value, handler)
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler)
+
+  const [text, setText] = useState('random title')
+  const handleOnClick = () => {
+    setText('gwe gwe gwe');
+  }
+  return <React.Fragment>
+            <button type='button' className='btn' onClick={handleOnClick}>Change Title</button>
+            <h1>{ text }</h1>
+         </React.Fragment>;
 };
 
 export default UseStateBasics;
