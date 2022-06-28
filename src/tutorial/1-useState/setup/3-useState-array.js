b66cccd48c556313a1811f6af361323955b7600e
masete <nicholasmasete72@gmail.com>
2022-06-28 16:47:36 +0300

    hooks in action

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -5,11 +5,12 @@ const UseStateArray = () => {
   const [people, setPeople] = React.useState(data)
   return  <>
             {people.map((person)=> {
-              const {id, name} = person;
-              return <div key='id' className='item'>
+              const { id, name } = person;
+              return <div key={id} className='item'>
                 <h4>{ name }</h4>
               </div>
             })}
+            <button className='btn' onClick={() => setPeople([])}>Clear list</button>
           </>;
 };
 
