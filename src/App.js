1cfca4d73ed1de46d196be0099b37b78a2b95771
masete <nicholasmasete72@gmail.com>
2022-06-28 17:35:37 +0300

    useState with objects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import Setup from './tutorial/1-useState/setup/3-useState-array'
+import React, { useState } from 'react'
+import Setup from './tutorial/1-useState/setup/4-useState-object'
 
 function App() {
-  return (
-    <div className='container'>
-      <Setup />
-    </div>
-  )
+  const[people, setPeople] = useState({
+    name: 'peter', 
+    age: 24, 
+    message:'random message'})
+
+  const changeEssage = () =>{
+    setPeople({...people, message:'hello world'})
+  }
+  return <>
+            <h3>{people.name}</h3>
+            <h3>{people.age}</h3>
+            <h3>{people.message}</h3>
+            <button className='btn' onClick={changeEssage}>Change message</button>
+  </>
 }
 
 export default App
