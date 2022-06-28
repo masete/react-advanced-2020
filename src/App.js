1bc099448518a9ff9fcc5761cece744cbbc4162f
masete <nicholasmasete72@gmail.com>
2022-06-28 17:46:40 +0300

    useState with objects done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Setup from './tutorial/1-useState/setup/4-useState-object'
 
 function App() {
-  const[people, setPeople] = useState({
-    name: 'peter', 
-    age: 24, 
-    message:'random message'})
-
-  const changeEssage = () =>{
-    setPeople({...people, message:'hello world'})
-  }
-  return <>
-            <h3>{people.name}</h3>
-            <h3>{people.age}</h3>
-            <h3>{people.message}</h3>
-            <button className='btn' onClick={changeEssage}>Change message</button>
-  </>
+  return (
+    <div className='container'>
+      <Setup />
+    </div>
+  )
 }
 
 export default App
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const[people, setPeople] = useState({
+    name: 'peter', 
+    age: 24, 
+    message:'random message'})
+
+  const changeEssage = () =>{
+    setPeople({...people, message:'hello world'})
+  }
+  return <>
+            <h3>{people.name}</h3>
+            <h3>{people.age}</h3>
+            <h3>{people.message}</h3>
+            <button className='btn' onClick={changeEssage}>Change message</button>
+  </>
 };
 
 export default UseStateObject;
