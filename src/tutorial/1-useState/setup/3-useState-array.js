1093e80791eb7e29f19fbad97e4226732a5257d0
masete <nicholasmasete72@gmail.com>
2022-06-28 17:10:39 +0300

    closing usestate with arrays

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,11 +3,16 @@ import { data } from '../../../data';
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data)
+  const deleteItem = (id) =>{
+    let newPeople = people.filter((person)=> person.id !== id);
+    setPeople(newPeople);
+  }
   return  <>
             {people.map((person)=> {
               const { id, name } = person;
               return <div key={id} className='item'>
                 <h4>{ name }</h4>
+                <button className='btn' onClick={()=>deleteItem(id)}>Delete item</button>
               </div>
             })}
             <button className='btn' onClick={() => setPeople([])}>Clear list</button>
