6a178cc0f66ef65fe563a03945b3d9a3ecb33f9b
masete <nicholasmasete72@gmail.com>
2022-06-30 23:40:03 +0300

    Starting useeffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
+  console.log('callUseEffect')
   return <h2>useEffect Basics</h2>;
+  console.log('render Component')
 };
 
 export default UseEffectBasics;
